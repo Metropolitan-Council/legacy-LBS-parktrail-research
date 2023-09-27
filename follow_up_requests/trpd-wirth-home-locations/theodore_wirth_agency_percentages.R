e1559779d343d2fd9ce799b1f6786d82229ca328
Raven McKnight <raven.i.mcknight@gmail.com>
2023-09-27 10:51:30 -0500

    comment & tidy code

--- follow_up_requests/trpd-wirth-home-locations/theodore_wirth_agency_percentages.R
+++ follow_up_requests/trpd-wirth-home-locations/theodore_wirth_agency_percentages.R
@@ -1,3 +1,5 @@
+##### setup #####
+
 ## source basic utility functions, colors for plotting, etc
 source(file.path(here::here(), "R/_load_packages.R"))
 source(file.path(here(), "R/_utility_functions.R"))
@@ -5,11 +7,12 @@ source(file.path(here(), "R/_global_parameters.R"))
 source(file.path(here(), "R/_streetlight_credentials_parameters.R"))
 source(file.path(here(), "R/_set_aesthetics.R"))
 
-
+## read data
 meta <- readxl::read_xlsx("data-processed/unit_metadata_2023.07.xlsx")
 
 theo <- meta %>%
   filter(unit_name == "Theodore Wirth, MPRB")
+# theodore wirth's shp_id = "p_metro_25"
 
 home_bg <- sf::read_sf("data-processed/home-locations/park-home-block-groups/park-home-block-groups.shp") %>%
   st_transform(4269)
@@ -19,15 +22,19 @@ theo_home_bg <- home_bg %>%
          !!theo$shp_id) %>%
   filter(!is.na(p_metro_25))
 
+##### get geographies ####
 
+## state
 mn_geo <- tigris::states() %>%
   filter(NAME == "Minnesota") %>%
   st_transform(4269)
 
+## minneapolis
 minneapolis_geo <- tigris::places("MN") %>%
   filter(NAME == "Minneapolis") %>%
   st_transform(4269)
 
+## 7-county metro
 metro_geo <- tigris::counties("MN") %>%
   st_transform(4269) %>%
   filter(NAME %in% c("Anoka",
@@ -40,6 +47,7 @@ metro_geo <- tigris::counties("MN") %>%
   ) %>%
   st_union()
 
+## greater mn (state minus 7-county metro)
 gmn_geo <- st_difference(mn_geo, st_union(st_combine(metro_geo)), dimension = "polygon") %>%
   st_make_valid()
 
@@ -47,22 +55,26 @@ gmn_geo <- st_difference(mn_geo, st_union(st_combine(metro_geo)), dimension = "p
 ia_geo <- read_sf("/Volumes/shared/CommDev/Research/Public/GIS/Parks/Park_Operating_Agencies.shp")  %>%
   st_transform(4269)
 
-## suburban Hennepin County (all Hennepin Co minus Minneapolis, but including Bloomington)
+## suburban Hennepin County (all of Hennepin minus Minneapolis, but including Bloomington)
 hennepin_geo <- tigris::counties(state = "MN") %>%
   st_transform(4269) %>%
   filter(NAME == "Hennepin")
 suburban_hennepin_geo <- st_difference(hennepin_geo, st_union(st_combine(minneapolis_geo)), dimension = "polygon") %>%
   st_make_valid()
 
-#### block groups don't nest perfectly
-# get proportion of bg in each region to make sure things sum to 100%
+##### assign block groups to geographies #####
+## block groups don't nest perfectly, so get proportion of bg in each geography to make sure things sum to 100%
+
+## block groups DO nest into the state perfectly
 mn_bgs <- tigris::block_groups("MN", cb = TRUE) %>%
   mutate(bg_area = st_area(.))
 
+## get area of each block group
 mn_bg_area <- mn_bgs %>%
   st_drop_geometry() %>%
   select(GEOID, bg_area)
 
+## all non-mn block groups
 out_of_state_bgs <- theo_home_bg %>%
   filter(!bg_id %in% mn_bgs$GEOID) %>%
   select(GEOID = bg_id) %>%
@@ -70,12 +82,14 @@ out_of_state_bgs <- theo_home_bg %>%
   st_drop_geometry() %>%
   mutate(bg_in_region = 1)
 
+## in-state block groups
 in_state_bgs <- mn_bgs %>%
   select(GEOID) %>%
   mutate(region = "Minnesota") %>%
   st_drop_geometry() %>%
   mutate(bg_in_region = 1)
 
+## 7-county metro block groups
 metro_bgs <- st_intersection(mn_bgs, metro_geo) %>%
   st_make_valid() %>%
   mutate(bg_in_region = as.numeric(st_area(.)/bg_area)) %>%
@@ -83,6 +97,7 @@ metro_bgs <- st_intersection(mn_bgs, metro_geo) %>%
   mutate(region = "7-County Metropolitan Region") %>%
   st_drop_geometry()
 
+## greater mn block groups
 gmn_bgs <- st_intersection(mn_bgs, gmn_geo) %>%
   st_make_valid() %>%
   mutate(bg_in_region = as.numeric(st_area(.)/bg_area)) %>%
@@ -90,6 +105,7 @@ gmn_bgs <- st_intersection(mn_bgs, gmn_geo) %>%
   mutate(region = "Greater Minnesota (outside of 7-County Metro)") %>%
   st_drop_geometry()
 
+## minneapolis block groups
 minneapolis_bgs <- st_intersection(mn_bgs, minneapolis_geo) %>%
   st_make_valid() %>%
   mutate(bg_in_region = as.numeric(st_area(.)/bg_area)) %>%
@@ -97,6 +113,7 @@ minneapolis_bgs <- st_intersection(mn_bgs, minneapolis_geo) %>%
   mutate(region = "Minneapolis") %>%
   st_drop_geometry()
 
+## suburban hennepin block groups
 suburban_hennepin_bgs <- st_intersection(mn_bgs, suburban_hennepin_geo) %>%
   st_make_valid() %>%
   mutate(bg_in_region = as.numeric(st_area(.)/bg_area)) %>%
@@ -104,13 +121,14 @@ suburban_hennepin_bgs <- st_intersection(mn_bgs, suburban_hennepin_geo) %>%
   mutate(region = "Suburban Hennepin County (Hennepin County minus Minneapolis, including Bloomington)") %>%
   st_drop_geometry()
 
+## implementing agency block groups (this is where finding the proportions has the biggest impact)
 ia_bgs <- st_intersection(mn_bgs, ia_geo) %>%
   st_make_valid() %>%
   mutate(bg_in_region = as.numeric(st_area(.)/bg_area)) %>%
   select(region = Map_Label3, GEOID, bg_in_region) %>%
   st_drop_geometry()
 
-##### combine
+##### combine results #####
 all_bgs <- bind_rows(out_of_state_bgs,
                      in_state_bgs,
                      metro_bgs,
@@ -127,12 +145,13 @@ all_bgs <- bind_rows(out_of_state_bgs,
   group_by(region) %>%
   summarise(visitation_from_region = sum(pct_visits_from_region))
 
+##### distribute estimated annual visits based on % from home location #####
+## we did not publicly share estimated counts by home location, so this is the most reproducible approach
 load(file.path(here(), "data-intermediate/processed/park-volume.rda"))
 theo_21_visitation <- annual_volume %>%
   filter(year == 2021, zone_name == "Theodore-Wirth_park_Metro-Regional_MPRB")
 
-
-
+## format results
 results <- all_bgs %>%
   transmute(Region = region,
             `Estimated visits` = round(visitation_from_region * theo_21_visitation$annual_total),
@@ -144,7 +163,7 @@ results <- all_bgs %>%
                                             sort(ia_geo$Map_Label3)))) %>%
   arrange(Region)
 
-
+##### create output excel workbook ######
 metadata_sheet <- tibble(
   "Metadata" = c(
     "This dataset contains information about visitation to Theodore Wirth Regional Park as estimated by location based services data (LBS). LBS data was provided by StreetLight Data, Inc and accessed in July 2023. LBS data is an aggregated and anonymized data source providing information about when and where people travel. Users are advised that LBS data provided by StreetLight uses a sample based approach, and thus all numbers provided are estimates. More information about StreetLight's methodology can be found on their website [https://www.streetlightdata.com/].",
@@ -176,99 +195,6 @@ addWorksheet(wb, "results")
 writeData(wb, sheet = 2, x = results)
 setColWidths(wb, sheet = 2, cols = c(1:ncol(results)), widths = "auto")
 
-saveWorkbook(wb, file.path(here::here(), "follow_up_requests/theodore_wirth_visitor_home_locations.xlsx"),
+saveWorkbook(wb, file.path(here::here(), "follow_up_requests/trpd-wirth-home-locations/theodore_wirth_visitor_home_locations.xlsx"),
              overwrite = TRUE
 )
-
-# theo_mpls <- st_intersection(theo_home_bg, minneapolis)
-# theo_metro <- st_intersection(theo_home_bg, metro)
-# theo_mn <- st_intersection(theo_home_bg, mn)
-# theo_suburb_henn <- st_intersection(theo_home_bg, suburban_hennepin)
-# theo_ia <- st_intersection(theo_home_bg, ia)
-
-# # about 43% from block groups in Minneapolis
-# sum(theo_mpls$p_metro_25)
-#
-# # and 52% from suburban Hennepin
-# sum(theo_suburb_henn$p_metro_25)
-#
-# # about 97% in metro (~3% Greater MN)
-# sum(theo_metro$p_metro_25)
-#
-# # and 98.9% in state
-# sum(theo_mn$p_metro_25)
-
-# theo_ia_pcts <- theo_ia %>%
-#   group_by(Map_Label) %>%
-#   summarise(pct = sum(p_metro_25))
-#
-# sum(theo_ia_pcts$pct)
-
-## block groups don't nest perfectly into agency boundaries
-# first, intersect block groups with agencies to get % of bg per agency
-# bgs <- tigris::block_groups("MN", year = 2020)
-# bg_areas <- bgs %>%
-#   select(GEOID) %>%
-#   mutate(bg_area = st_area(.)) %>%
-#   st_drop_geometry()
-# bg_ia <- st_intersection(bgs, ia)
-#
-# bg_ia_prop <- bg_ia %>%
-#   select(GEOID, Map_Label) %>%
-#   mutate(area = st_area(.)) %>%
-#   left_join(bg_areas) %>%
-#   mutate(prop_bg_in_ia = area/bg_area)
-#
-# theo_bg_ia <- bg_ia_prop %>%
-#   full_join(theo_home_bg %>%
-#               st_drop_geometry() %>%
-#               rename(GEOID = bg_id)) %>%
-#   filter(!is.na(p_metro_25)) %>%
-#   mutate(visits_in_ia = prop_bg_in_ia * p_metro_25) %>%
-#   group_by(Map_Label) %>%
-#   summarise(pct = sum(visits_in_ia)) %>%
-#   filter(!is.na(Map_Label))
-#
-# sum(theo_bg_ia$pct)
-#
-#
-# ## combine
-#
-# instate <- theo_mn %>%
-#   st_drop_geometry() %>%
-#   mutate(region = "Minnesota") %>%
-#   group_by(region) %>%
-#   summarise(percent_visitation = sum(p_metro_25))
-#
-# out_of_state <- data.frame(region = "Out of state",
-#                            percent_visitation = 1 - instate$percent_visitation)
-#
-# minneapolis <- theo_mpls %>%
-#   st_drop_geometry() %>%
-#   mutate(region = "Minneapolis") %>%
-#   group_by(region) %>%
-#   summarise(percent_visitation = sum(p_metro_25))
-#
-# suburban_hennepin <- theo_suburb_henn %>%
-#   st_drop_geometry() %>%
-#   mutate(region = "Suburban Hennepin County (Hennepin County minus Minneapolis)") %>%
-#   group_by(region) %>%
-#   summarise(percent_vistation = sum(p_metro_25))
-#
-# greater_mn <- st_difference(theo_home_bg, metro)
-#
-# metro <- theo_metro %>%
-#   st_drop_geometry() %>%
-#   mutate(region = "7-County Metropolitan Area") %>%
-#   group_by(region) %>%
-#   summarise(percent_visitation = sum(p_metro_25))
-#
-#
-# ias <- theo_bg_ia %>%
-#   st_drop_geometry() %>%
-#   rename(region = Map_Label) %>%
-#   group_by(region) %>%
-#   summarise(percent_visitation = sum(pct))
-#
-# sum(ias$percent_visitation)
-# metro$percent_visitation
